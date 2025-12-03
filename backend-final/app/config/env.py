1ca2668a663867a549431d2a897742c39242c8d5
dylanhamidy <dylanhamidy0909@gmail.com>
2025-12-03 11:19:39 +0900

    resolve CORS origin

--- backend-final/app/config/env.py
+++ backend-final/app/config/env.py
@@ -4,10 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL= os.getenv("DATABASE_URL")
+FRONTEND_URL=  os.getenv("FRONTEND_URL")
+
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in environment variables")
 
 class Envs:
     DATABASE_URL: str = DATABASE_URL
+    FRONTEND_URL: str = FRONTEND_URL
     
 envs = Envs()
--- backend-final/app/middleware/middleware.py
+++ backend-final/app/middleware/middleware.py
@@ -1,18 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.config.env import envs
+
 def setup_middleware(app: FastAPI):
     """
     Configure all middleware for the FastAPI application.
     """
     
+    frontend_origins = envs.FRONTEND_URL
+    if not frontend_origins:
+        raise ValueError("FRONTEND_URL environment variable is not set")
+    
+    
     #CORS Middleware to connect to frontend
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            "http://localhost:5173",  # React dev server
-            "http://localhost:8000",  # FastAPI docs
-        ],
+        allow_origins= frontend_origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
